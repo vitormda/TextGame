b4d2a1f95e4cd6725589c301168a3c876f93180e
Vitor Moro de Almeida <vitor1811@gmail.com>
2017-08-16 17:01:58 -0300

    Esboço inicial de história e início dos testes com XML

--- app/src/main/java/br/go/cdg/textgame/MainActivity.java
+++ app/src/main/java/br/go/cdg/textgame/MainActivity.java
@@ -2,6 +2,10 @@ package br.go.cdg.textgame;
 
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
+import android.util.Log;
+
+import java.io.IOException;
+import java.io.InputStream;
 
 public class MainActivity extends AppCompatActivity {
 
@@ -9,5 +13,17 @@ public class MainActivity extends AppCompatActivity {
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
+
+        try {
+            InputStream is = getAssets().open("historias.xml");
+
+            int length = is.available();
+            byte[] data = new byte[length];
+            is.read(data);
+            Log.i("DATA > ", (new String(data)).toString());
+
+        } catch (IOException e) {
+            e.printStackTrace();
+        }
     }
 }
